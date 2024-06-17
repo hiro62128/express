8cf57025fdd95d2e9537c33b1bfbe3b5de595bac
hayashi <hirotaka0128@gmail.com>
2024-06-17 21:37:01 +0900

    add hello page

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET hello page. */
 router.get('/hello', function(req, res, next) {
-  res.render('index', { title: 'Hello Page' });
+  res.render('hello', { title: 'Hello Page' });
 });
 
 module.exports = router;
